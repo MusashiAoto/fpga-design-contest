ece2e7e44f55f7eb3d151a952f632e3bd9e8baa9
Musashi Aoto <15j5002@stu.meisei-u.ac.jp>
2019-11-06 13:11:44 +0900

    update

--- camera_algorithm/move_test.py
+++ camera_algorithm/move_test.py
@@ -4,23 +4,6 @@ import time
 
 
 
-def chokan(img):
-    img1=img
-    height=240
-    width=320
-    image = np.zeros((700, 700, 3), np.uint8)
-    src = np.array([[100,40],[210,40],[0,239],[width,239]],np.float32)
-    dst = np.array([[0,0],[500,0],[int(500/5),800],[int(500/5*4),800]],np.float32)
-
-    M = cv2.getPerspectiveTransform(src, dst)
-    warp = cv2.warpPerspective(img1.copy(), M, (500, 800))
-
-    return warp
-    #cv2.imshow('transform', warp)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-    #cv2.imwrite("convert.png",warp)
-
 
 fractal_controll=0
 minLineLength = 1000
@@ -31,13 +14,33 @@ Xpoint=145
 avecontrast=0
 contraststate=0
 fps=30
-Height=240
-Width=320
+Height=800
+Width=500
 
 
-cap = cv2.VideoCapture("../output2.m4v")
+def chokan(img):
+    img1=img
+    width=320
+
+    #image = np.zeros((700, 700, 3), np.uint8)
+    src = np.array([[100,40],[210,40],[0,239],[width,239]],np.float32)
+    dst = np.array([[0,0],[Width,0],[int(Width/5),Height],[int(Width/5*4),Height]],np.float32)
+
+    M = cv2.getPerspectiveTransform(src, dst)
+    warp = cv2.warpPerspective(img1.copy(), M, (Width, Height))
+
+    return warp
+    #cv2.imshow('transform', warp)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+    #cv2.imwrite("convert.png",warp)
+
+
+
+
+cap = cv2.VideoCapture("test_movie.m4v")
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v') 
-convert_out = cv2.VideoWriter('output2.m4v',fourcc, fps, (500,800))
+convert_out = cv2.VideoWriter('output2.m4v',fourcc, fps, (Width,Height))
 
 
 
@@ -52,5 +55,13 @@ while(cap.isOpened()):
     if ret==False:
         break
     out=chokan(img1)
+
+    out= cv2.line(out,(int(Width/4),0),(int(Width/4),Height),(255,0, 0),5)
+    out= cv2.line(out,(0,int(Height/4*3)),(Width,int(Height/4*3)),(255,0, 0),5)
+    
+    out= cv2.line(out,(int(Width/4*3),0),(int(Width/4*3),Height),(0,255, 0),5)
+    out= cv2.line(out,(0,int(Height/2)),(Width,int(Height/2)),(0,255, 0),5)
+    
+
     convert_out.write(out)
 convert_out.release()

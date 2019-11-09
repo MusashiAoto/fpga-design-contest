934103b476ea7fb4ee52862b02184d09ac0af93f
Musashi Aoto <15j5002@stu.meisei-u.ac.jp>
2019-11-09 16:48:53 +0900

    threshlod

--- camera_algorithm/move_test.py
+++ camera_algorithm/move_test.py
@@ -20,10 +20,17 @@ Width=500
 
 def chokan(img):
     img1=img
-    width=320
+    height, width, channels = img.shape[:3]
+    hidariueX=int(width/320*100)
+    hidariueY=int(height/240*40)
+    migiueX=int(width/320*210)
+    migiueY=int(height/240*40)
+
 
     #image = np.zeros((700, 700, 3), np.uint8)
-    src = np.array([[100,40],[210,40],[0,239],[width,239]],np.float32)
+    #src = np.array([[100,40],[210,40],[0,239],[width,239]],np.float32)
+    src = np.array([[hidariueX,hidariueY],[migiueX,migiueY],[0,height],[width,height]],np.float32)
+
     dst = np.array([[0,0],[Width,0],[int(Width/5),Height],[int(Width/5*4),Height]],np.float32)
 
     M = cv2.getPerspectiveTransform(src, dst)
@@ -38,11 +45,11 @@ def chokan(img):
 
 
 
-cap = cv2.VideoCapture("test_movie.m4v")
+cap = cv2.VideoCapture("roadtest.mov")
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v') 
 convert_out = cv2.VideoWriter('output2.m4v',fourcc, fps, (Width,Height))
 
-before=160
+before=230
 after=0
 
 
@@ -65,24 +72,25 @@ while(cap.isOpened()):
     if ret==False:
         break
     out=chokan(img1)
+    imm= out
     out  = cv2.cvtColor(out, cv2.COLOR_BGR2GRAY)
     ret2, otsu = cv2.threshold(out, 0, 255, cv2.THRESH_OTSU)
-    if abs(ret2-before)>50:
+    if abs(ret2-before)>30:
         ret,  out= cv2.threshold(out, before, 255, cv2.THRESH_BINARY)
     else:
         before=ret2
         out=otsu
     #print(ret2)
-
+    
 
 
     curvedetect()
     out = cv2.cvtColor(out, cv2.COLOR_GRAY2BGR)
-    #out= cv2.line(out,(int(Width/4),int(Height/2)),(int(Width/4),Height),(255,0, 0),5)
-    #out= cv2.line(out,(0,int(Height/4*3)),(Width,int(Height/4*3)),(255,0, 0),5)
+    # out = cv2.line(out,(int(Width/4),int(Height/2)),(int(Width/4),Height),(255,0, 0),5)
+    # out = cv2.line(out,(0,int(Height/4*3)),(Width,int(Height/4*3)),(255,0, 0),5)
     
-    #out= cv2.line(out,(int(Width/4*3),0),(int(Width/4*3),Height),(0,255, 0),5)
-    #out= cv2.line(out,(0,int(Height/2)),(Width,int(Height/2)),(0,255, 0),5)
+    # out = cv2.line(out,(int(Width/4*3),0),(int(Width/4*3),Height),(0,255, 0),5)
+    # out = cv2.line(out,(0,int(Height/2)),(Width,int(Height/2)),(0,255, 0),5)
 
     cv2.imshow("window_name", out)
     #time.sleep(0.05)
